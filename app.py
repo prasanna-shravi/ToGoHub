95a08925a2432b232d5dbb75b420e70e27e9770b
kimelikangogo <102039633+kimelikangogo@users.noreply.github.com>
2022-04-22 16:27:31 +0100

    Updating User Table

--- app.py
+++ app.py
@@ -10,7 +10,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 #Using SQLAlchemy to create DB models
 db = SQLAlchemy(app)
 
+
 #DB Models for SQL Tables in our Database
+#User table 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    orders=db.relationship('Order', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return f'User <{self.name}>'
 
 
 #Flask API routes
